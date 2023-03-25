14b804c3147acb22bd6d83aaeb1aeef26cc7e959
Paul Wright <31939113+plwt@users.noreply.github.com>
2023-03-25 20:31:18 +0000

    Update timer.py

--- timer.py
+++ timer.py
@@ -10,11 +10,21 @@ def printTime(self):
     (hours, minutes) = divmod(minutes, 60)  
     (days,hours) = divmod(hours, 24)
     #what to do comes next, I serial printed it for now  
-    ti = print(str(days)+":"+f"{hours:02d}"+":"+f"{minutes:02d}"+":"+f"{seconds:02d}")  
+    print(str(days)+":"+f"{hours:02d}"+":"+f"{minutes:02d}"+":"+f"{seconds:02d}")  
 
 timer=Timer(-1)  
 delay = 10 #in seconds  
 timer.init(period=delay*1000, mode=Timer.PERIODIC, callback=printTime)  
 #default is a dummy placeholder,
 #the timer callback sends a 'self' value which caused an error during testing  
-printTime('default')  
+printTime('default') 
+
+
+
+
+
+
+
+
+
+
