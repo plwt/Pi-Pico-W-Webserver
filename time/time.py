beb80841dcf5690df1d78ee78ab1c6c50b51844d
Paul Wright <31939113+plwt@users.noreply.github.com>
2023-04-02 00:15:51 +0100

    Create time.py

---
+++ time/time.py
@@ -0,0 +1,75 @@
+import network
+import socket
+from time import sleep
+from picozero import pico_temp_sensor
+import machine
+
+
+ssid = 'ADDIDHERE'
+password = 'ADDPASSWORDHERE'
+
+
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        sleep(1)
+    ip = wlan.ifconfig()[0]
+    print(f'Connected on {ip}')
+    return ip
+    
+
+def open_socket(ip):
+    # Open a socket
+    address = (ip, 80)
+    connection = socket.socket()
+    connection.bind(address)
+    connection.listen(1)
+    returnne
+
+
+def webpage(temperature, time):
+    #Template HTML
+    html = f"""
+            <!DOCTYPE html>
+            <html>
+	          <body style="background-color:#000000">
+            <p> </p>
+	          <p style="color:#00ff41">Temperature is {temperature}</p>
+            <p> </p>
+            <p style="color:#00ff41">Timr<a href="https://plwt.github.io/tfpico.html">But whaare you really?</a></p> 
+            </body>
+            </html>
+            """
+    return str(html)
+
+
+def serve(connection):
+    #Start a webserver
+    while True:
+        client = connection.accept()[0]
+        request = client.recv(1024)
+        request = str(request)
+        try:
+            request = request.split()[1]
+        except IndexError:
+            pass
+        temperature = pico_temp_sensor.temp
+        #Get the current time
+        current_time = utime.localtime()
+        #Format the current time as "dd/mm/yyyy HH:MM"
+        formatted_time = "{:02d}/{:02d}/{} {:02d}:{:02d}".format(current_time[2], current_time[1], current_time[0],
+        html = webpage(temperature, formatted_time)
+        client.send(html)
+        client.close()
+
+
+try:
+    ip = connect()
+    connection = open_socket(ip)
+    serve(connection)
+except KeyboardInterrupt:
+    machine.reset()
