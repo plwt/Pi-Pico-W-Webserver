d29a3b17ef60e1e37395a6a074918a30906f2f85
Paul Wright <31939113+plwt@users.noreply.github.com>
2023-04-10 16:39:19 +0100

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,23 +1,13 @@
-import machine
-from machine import Timer  
-import time  
+import utime
 
-timeInit = time.time()  
-
-def printTime(self):  
-    timeDiff = time.time()-timeInit  
-    (minutes, seconds) = divmod(timeDiff, 60)  
-    (hours, minutes) = divmod(minutes, 60)  
-    (days,hours) = divmod(hours, 24)
-    #what to do comes next, I serial printed it for now  
-    print(str(days)+":"+f"{hours:02d}"+":"+f"{minutes:02d}"+":"+f"{seconds:02d}")  
-
-timer=Timer(-1)  
-delay = 10 #in seconds  
-timer.init(period=delay*1000, mode=Timer.PERIODIC, callback=printTime)  
-#default is a dummy placeholder,
-#the timer callback sends a 'self' value which caused an error during testing  
-printTime('default') 
+def uptime_in_hours():
+    start_time = utime.ticks_ms()
+    while True:
+        current_time = utime.ticks_ms()
+        uptime_ms = current_time - start_time
+        uptime_hours = uptime_ms / (1000 * 60 * 60)
+        print("Uptime: {:.2f} hours".format(uptime_hours))
+        utime.sleep(1)
 
 
 
