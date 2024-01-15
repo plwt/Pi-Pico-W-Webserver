c922df3b609652269cccc5e739b9003d3608b197
Paul Wright <31939113+plwt@users.noreply.github.com>
2024-01-15 21:31:11 +0000

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,37 +1,71 @@
+# ================================================== 
+""" 
+Project objectives:
+    Setup the Raspberry Pi Pico W as a web server
+    Display onboard temperature in a webpage
+    Display onboard LED status and control it via a webpage
+
+Author: Adrian Josele G. Quional
+
+Code reference: https://projects.raspberrypi.org/en/projects/get-started-pico-w/0
+"""
+# ==================================================
+
+# modules
 import network
 import socket
 from time import sleep
-from picozero import pico_temp_sensor
-from machine import ADC
+from picozero import pico_temp_sensor, pico_led
+import machine
 
-ssid = 'ADDIDHERE'
-password = 'ADDPASSWORDHERE'
-
+# connection details
+ssid = "??????"
+password = "??????"
 
 def connect():
-    #Connect to WLAN
+    """This function facilitates connection of Raspberry Pi Pico W to the Internet using WiFi."""
+    
+    # connecting to WLAN
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(ssid, password)
+    
+    # performing handshake
+    # continuously wait for connection
     while wlan.isconnected() == False:
         print('Waiting for connection...')
         sleep(1)
+        
+    # displaying to the console the IP address that the Raspberry Pi Pico is assigned to
     ip = wlan.ifconfig()[0]
-    print(f'Connected on {ip}')
-    return ip
+    print(f"Connected on {ip}")
     
+    return ip
 
 def open_socket(ip):
-    # Open a socket
+    """
+    This function opens a socket in order for the server (Raspberry Pi Pico W)
+    to be able to listen to a client (any web browser accessing the IP address
+    of the Raspberry Pi Pico W).
+    """
+    
+    # opening a socket
     address = (ip, 80)
     connection = socket.socket()
     connection.bind(address)
     connection.listen(1)
+    
     return connection
 
-
-def webpage(temperature):
-    # Template HTML
+def webpage(temperature, state):
+    """
+    This function contains the HTML string containing elements that would be displayed in the webpage.
+    
+    As a sample, this takes in temperature (from the onboard temperature sensor) and the onboard LED state (ON or OFF).
+    According to the actual purpose of the project, the parameters can be changed.
+    """
+    
+    
     html = f"""
             <!DOCTYPE html>
             <html>
@@ -42,39 +76,51 @@ def webpage(temperature):
             </body>
             </html>
             """
+    
     return str(html)
 
-
-# Temperature Sensor
-sensor_temp = machine.ADC(4)
-conversion_factor = 3.3 / (65535)
- 
-def temperature():
-    temperature_value = sensor_temp.read_u16() * conversion_factor 
-    temperature_Celcius = (27 - (temperature_value - 0.706)/0.00172169) - 4 
-    utime.sleep(2)
-    return temperature_Celcius
-
-
 def serve(connection):
-    #Start a webserver
+    """This function starts the web server and serves the webpage."""
+    
+    # starting the web server
+    
+    # initially, LED should be OFF
+    state = 'OFF'
+    pico_led.off()
+    
+    # temperature is initially set to 0 (just for initialization purposes)
+    temperature = 0
+    
     while True:
         client = connection.accept()[0]
         request = client.recv(1024)
         request = str(request)
+        
         try:
             request = request.split()[1]
         except IndexError:
             pass
-        temperature = temperature_Celcius
-	      html = webpage(temperature)
+        
+        # controls LED state depending on request
+        if request == '/lighton?':
+            pico_led.on()
+            state = 'ON'
+        elif request =='/lightoff?':
+            pico_led.off()
+            state = 'OFF'
+        
+        # gets onboard temperature via picozero module
+        # stores onboard temperature in the temperature variable
+        temperature = pico_temp_sensor.temp - 9
+        
+        html = webpage(temperature, state)
         client.send(html)
         client.close()
-
 
 try:
     ip = connect()
     connection = open_socket(ip)
     serve(connection)
+
 except KeyboardInterrupt:
     machine.reset()
